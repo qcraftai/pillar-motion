0df95bfeadfc805a2757be5414d2c2926e16ed15
Chenxu Luo <chenxuluo@jhu.edu>
2021-11-15 10:19:28 -0500

    fix aug

--- motion/core/sampler/preprocess.py
+++ motion/core/sampler/preprocess.py
@@ -2,22 +2,17 @@ import abc
 import sys
 import time
 from collections import OrderedDict
-from functools import reduce
-
 
 import numpy as np
 
-
-import copy
-
-
-def random_flip_xy(points, trans, probability=0.5):
+def random_flip_xy(points_list, trans, probability=0.5):
     # x flip
     enable = np.random.choice(
         [False, True], replace=False, p=[1 - probability, probability]
     )
     if enable:
-        points[:, 1] = -points[:, 1]
+        for points in points_list:
+            points[:, 1] = -points[:, 1]
         back = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
         for i in range(6):
             trans[i] = trans[i] @ back
@@ -30,13 +25,14 @@ def random_flip_xy(points, trans, probability=0.5):
         back = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
         for i in range(6):
             trans[i] = trans[i] @ back
-    return points, trans
+    return points_list, trans
 
 
-def global_scaling(points, trans, min_scale=0.95, max_scale=1.05):
+def global_scaling(points_list, trans, min_scale=0.95, max_scale=1.05):
     noise_scale = np.random.uniform(min_scale, max_scale)
-    points[:, :3] *= noise_scale
+    for points in points_list:
+        points[:, :3] *= noise_scale
     back = np.array([[1./noise_scale, 0, 0, 0], [0, 1./noise_scale, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     for i in range(6):
         trans[i] = trans[i] @ back
-    return points, trans
+    return points_list, trans
